6fb5580bc6862b2fe1df583c7dfa117e61af9f37
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2023-08-15 11:57:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,7 @@ from pyrogram.types import *
 @Client.on_message(filters.command("start") & filters.private)
 async def start_handle(_, m):
   user = m.from_user
-  START_MSG = ''' HEY {user.mention} Welcome \n I am Movies Filter Bot\n '''
+  START_MSG = f''' HEY {user.mention} Welcome \n I am Movies Filter Bot\n '''
   BUTTON = InlineKeyboardMarkup([[
     InlineKeyboardButton(text="Help", callback_data="help"),
     InlineKeyboardButton(text="Owner", user_id=OWNER_ID)
