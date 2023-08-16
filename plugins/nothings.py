050fb5af0155c1328d3c471a43369ef1f75ce3a0
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-16 17:07:20 +0530

    push

--- plugins/nothings.py
+++ plugins/nothings.py
@@ -19,12 +19,27 @@ async def info_handle(_, m):
   else:
     await m.reply(f"Hey {name} You Didn't Purchase Any Plan")
   
-@Client.on_message(filters.command("index"))
-async def index_handle(_,m):
-  chat_id = m.chat.id 
-  await m.reply("connecting ...")
-  
-@Client.on_message(filters.command("remove"))
-async def remove_handle(_, m): 
-  chat_id = m.chat.id
-  await m.reply("error")
+@Client.on_message(filters.command('leave') & filters.private &  filters.chat(OWNER_ID))
+async def leave_a_chat(bot, message):
+    if len(message.command) == 1:
+        return await message.reply('Give me a chat id')
+    chat = message.command[1]
+    try:
+        chat = int(chat)
+    except:
+        chat = chat
+    try:
+        buttons = [[
+            InlineKeyboardButton('𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url=f'https://t.me/YaaraOP')
+        ]]
+        reply_markup=InlineKeyboardMarkup(buttons)
+        await bot.send_message(
+            chat_id=chat,
+            text='<b>Hello Friends, \nMy admin has told me to leave from group so i go! If you wanna add me again contact my support group.</b>',
+            reply_markup=reply_markup,
+        )
+
+        await bot.leave_chat(chat)
+        await message.reply(f"left the chat `{chat}`")
+    except Exception as e:
+        await message.reply(f'Error - {e}')
