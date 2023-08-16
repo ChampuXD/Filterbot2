a63049baf6fe53effb518b73ea1dadf7b794a4c8
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-16 21:39:38 +0530

    fix

--- plugins/search.py
+++ plugins/search.py
@@ -9,7 +9,6 @@ import time
 import urllib.parse
 
 MESSAGE_LENGTH = 4096
-yk_time = 1
 
 ignore_words = ["in", "and", "hindi", "movie", "tamil", "telugu", "dub", "hd", "man", "series", "full", "dubbed", "kannada", "season", "part", "all", "2022", "2021", "2023", "1", "2", "3", "4", "5", "6", "7" ,"8", "9", "0", "2020", "2019", "2018" , "2017", "2016", "2014", "all", "new", "2013", "()", "movies", "2012", "2011", "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "1999", "1998", "1997", "1996", "1995", "-+:;!?*", "language", "480p", "720p", "1080p", "south", "Hollywood", "bollywood", "tollywood",] # words to ignore in search query
 
@@ -26,46 +25,43 @@ async def clean_query(query):
 
 @Client.on_message(filters.text & filters.group & filters.incoming & ~filters.command(["auth", "index", "id"]))
 async def search(bot, message):
-    start_time = time.time()  # Start measuring elapsed time
-    f_sub = await force_sub(bot, message)
-    if f_sub == False:
-        return
+  chat_id = message.chat.id
+  star = time.time()
+  f_sub = await force_sub(bot, message)
+  if f_sub == False:
+      return
 
-    channels = (await get_group(message.chat.id))["channels"]
-    if not channels:
-        return
+  channels = (await get_group(chat_id))["channels"]
+  if not channels:
+      return
 
-    if message.text.startswith("/"):
-        return
+  if message.text.startswith("/"):
+      return
+    
+    query = await clean_query(message.text)
+    query_words = query.split()
 
-    quer = await clean_query(message.text)
-    bas_yaar = quer.split()
     results = ""
-    try:
-        for channel in channels:
-          for query in bas_yaar:
-              async for msg in YaaraOP.search_messages(chat_id=channel, query=query):
-                  name = (msg.text or msg.caption).split("\n")[0]
-                  if name in results:
-                    continue
-                  yk_results = f" {name}\n {msg.link}\n\n"
-                  if len(results) + len(yk_results) > MESSAGE_LENGTH:
-                    await message.reply(f"{results}", disable_web_page_preview=True)
+    for chk in channels:
+        for word in query_words:
+            async for msg in yash.search_messages(int(chk), query=word, limit=10):
+                if msg.caption or msg.text:
+                    name = (msg.text or msg.caption).split("\n")[0]
+                    if name in results:
+                        continue
+                    new_results = f"{name}\n {msg.link}\n\n"
+                    if len(results) + len(new_results) > MESSAGE_LENGTH:
+                        await message.reply(f"{results}", disable_web_page_preview=True)
                     results = ""
-                    await asyncio.sleep(yk_time)
-                    results += yk_results 
-
-        if not results:
-          no_results_message = f"No Results Found 🔎 \n\n"
-            
-          msg = await message.reply_text(text=no_results_message, disable_web_page_preview=True)
-          _time = int(time()) + (2 * 60)
-          await save_dlt_message(msg, _time)
-          return
-
-        elapsed_time = time.time() - start_time
-        total_time = f"Searched in {elapsed_time:.2f} sec." # Add the duration to the footer
-        msg = await message.reply_text(text=results + total_time, disable_web_page_preview=True)
+                    results += new_results
+    
+    if results:
+        end = time.time()
+        omk = end - star
+        timee = f"Result Searched in {omk:.02} sec"
+        await message.reply(f" {results}\n {timee}", disable_web_page_preview=True)
+    else:
+        await message.reply("No Movie Found 🔎")
         _time = int(time()) + (120 * 60)
         await save_dlt_message(msg, _time)
     except Exception as e:
