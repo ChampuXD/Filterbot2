175e6d0bfe8d5b69a4686bb024c5312ba7382bc3
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-16 14:34:20 +0530

    ok

--- db.py
+++ db.py
@@ -23,8 +23,9 @@ async def add_group(group_id, group_name, user_name, user_id, channels, f_sub, v
        pass
 async def update_group(id, new_data, val):
     data = {"_id":id}
-    new_value = {"$set": {new_data, val}}
-    await grp_col.update_one(data, new_value)
+    new_value = {"$set":new_data}
+    dat = {"$set": val}
+    await grp_col.update_one(data, new_value,dat)
     
   
 async def delete_group(id):
