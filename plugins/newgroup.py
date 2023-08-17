a63c57a421e2deef44ae8220488feeb2cdda6aa2
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-17 20:36:37 +0530

    peer

--- plugins/newgroup.py
+++ plugins/newgroup.py
@@ -4,6 +4,7 @@ from asyncio import sleep
 from pyrogram import *
 from bot import * 
 
+omk = Client.resolve_peer(LOG_CHANNEL)
 @Client.on_message(filters.group & filters.new_chat_members)
 async def new_group(bot:Client, message):
     bot_id = (await bot.get_me()).id
@@ -19,6 +20,6 @@ async def new_group(bot:Client, message):
        m=await message.reply(f"Thanks for adding me in {message.chat.title} ✨")
        num_of_members = await bot.get_chat_members_count(message.chat.id) # get the number of members in the group
        text=f"#NewGroup\n\nGroup: {message.chat.title}\nGroupID: `{message.chat.id}`\nAddedBy: {message.from_user.mention}\nUserID: `{message.from_user.id}`\nNumber of Members: {num_of_members}"
-       await bot.send_message(chat_id=LOG_CHANNEL, text=text)
+       await bot.send_message(chat_id=omk, text=text)
        await sleep(60)
        await m.delete()
