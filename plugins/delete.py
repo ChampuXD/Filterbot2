01a7bb11885b0ca3689b9de94db7f0f521ce1ec3
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-18 18:50:06 +0530

    h

--- plugins/delete.py
+++ plugins/delete.py
@@ -1,10 +1,10 @@
 from time import time
 from datetime import datetime 
 import time 
-from bot import dbot as bot
+from bot import dbot
 from db import * 
 
-async def check_up(bot):
+'''async def check_up(bot):
     _time = int(time())  # Use int(time()) instead of int(time.time())
     all_data = await get_all_dlt_data(_time)
     for data in all_data:
@@ -16,9 +16,9 @@ async def check_up(bot):
         except Exception as e:
             print(f"Error deleting message_id {message_id} from chat_id {chat_id}: {str(e)}")
             pass
-    await delete_all_dlt_data(_time)
+    await delete_all_dlt_data(_time)'''
 
-async def delete_messages():
+async def main():
     while True:
         try:
             # Get current time
@@ -26,14 +26,14 @@ async def delete_messages():
             current_time = oki.replace(second=0,microsecond=0).strftime("%y-%m-%d %H:%M")
             print(current_time)
             d_find = del_find(current_time)
-            if current_time > d_find['time']:
+            if current_time >= d_find['time']:
               for dati in d_find:
                 print(dati)
                 chat_id = d_find["chat_id"]
                 message_id = d_find["message_id"]
 
                 # Delete the message
-                await bot.delete_messages(chat_id, message_id)
+                await dbot.delete_messages(chat_id, message_id)
 
                 # Remove the message data from MongoDB
                 del_col.delete_one({"_id": del_find["_id"]})
@@ -44,10 +44,11 @@ async def delete_messages():
         await asyncio.sleep(30)
 
 # Main function to run the check_up function
-async def run_check_up():
+'''async def run_check_up():
     async with bot:
         await check_up(bot)
-        await asyncio.sleep(1)
+        await asyncio.sleep(1)'''
             
 if __name__ == "__main__":
-    asyncio.get_event_loop().create_task(delete_messages())
+    asyncio.get_event_loop()
+    dbot.run()
