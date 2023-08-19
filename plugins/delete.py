3dbdc609c834354e19e82dcff38ff7966e485829
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-19 19:13:49 +0530

    ok

--- plugins/delete.py
+++ plugins/delete.py
@@ -2,7 +2,9 @@ from time import time
 from datetime import datetime 
 import time 
 from bot import dbot
-from db import * 
+from db import *
+from pyrogram import *
+
 
 '''async def check_up(bot):
     _time = int(time())  # Use int(time()) instead of int(time.time())
