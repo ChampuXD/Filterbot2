85cc7198687d5a29c9e752d062785705d54b722b
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-20 22:57:50 +0530

    ok

--- plugins/about.py
+++ plugins/about.py
@@ -4,3 +4,14 @@ from config import *
 from pyrogram import *
 from pyrogram.types import *
 
+TEXT = '''This bot is made by @owner_21 A full time python developer
+
+Our channel :- @movie_artss
+Our selling channel :- @platimostore
+
+Want to make any kink of bot & tool dm @owner_21'''
+
+@Client.on_message(filters.command("about"))
+async def about_handle(_,m):
+  chat_id = m.chat.id
+  await m.reply(TEXT)
