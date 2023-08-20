4c7534c74b40cfb13315c5e35403e6d756f83ce5
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-20 22:40:08 +0530

    okie

--- plugins/delete.py
+++ plugins/delete.py
@@ -6,6 +6,7 @@ from bot import dbot
 import time 
 from datetime import datetime 
 
+plan = ""
 async def check_up(bot):   
     _time = int(time.time()) 
     all_data = await get_all_dlt_data(_time)
@@ -35,7 +36,7 @@ async def plan_update():
         chat_id = data["_id"]
         user_id = data["user_id"]
         plan = data["plan"]
-        await update_group(id=chat_id, {"verified": False, "plan": ""})
+        await update_group(id=chat_id, {"verified": False, "plan": plan})
         msg = await dbot.send_message(chat_id, f"Your Plan Expired Today Now Contact To My Owner @{OWNER}")
         await dbot.pin_chat_message(
     chat_id,
