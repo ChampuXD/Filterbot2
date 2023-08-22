24d483b9e4060ed64de2f0fbcd9fe72ccd5a4f51
KashDaYash <85399597+KashDaYash@users.noreply.github.com>
2023-08-22 23:10:24 +0530

    Update auth.py

--- plugins/auth.py
+++ plugins/auth.py
@@ -27,15 +27,14 @@ async def chat_id_check(bot:Client, m):
 async def auth_handle(bot:Client, m):
   if m.text == "/auth":
     await m.reply("Please Provide Group ID And Time Period like /auth Group ID Time ")
-  chat_id = int(m.text.split(None,2)[1])
-  group = await get_group(chat_id)
+  id = int(m.text.split(None,2)[1])
+  group = await get_group(id)
   user_id = group["user_id"]
   user_name = group["user_name"]
   verified = group["verified"]
   if verified == True:
     await m.reply(f"user id: {user_id}\n username: @{user_name} group chat is already verified!")
   elif verified == False:
-    id = chat_id
     current_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
     end_time = int(m.text.split(None, 2)[-1])
     ok = current_date + timedelta(days=end_time)
@@ -43,7 +42,7 @@ async def auth_handle(bot:Client, m):
     timestamp = ok.strftime("%Y-%m-%d")
     await update_group(id, {"verified": True, "plan": current_time})
     await m.reply(f"user id: {user_id}\n username: @{user_name} group chat is verified!")
-    await bot.send_message(chat_id, f"hey @{user_name} Purchase A Plan For {end_time}days ")
+    await bot.send_message(id, f"hey @{user_name} Purchase A Plan For {end_time}days ")
   else:
     await m.reply("Verification Request Failed !!\nPlease Give Me Command in correct format\n **`/auth Group ID Time`**")
   
