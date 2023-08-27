419a56bf6431d434812cdce973867e82fba63481
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-27 18:31:16 +0530

    ok

--- plugins/delete.py
+++ plugins/delete.py
@@ -7,7 +7,7 @@ import time
 from datetime import datetime 
 
 PLAN = ""
-async def check_up(bot):   
+'''async def check_up(bot):   
   _time = int(time.time()) 
   all_data = await get_all_dlt_data(_time)
   for data in all_data:
@@ -22,7 +22,7 @@ async def check_up(bot):
 
 
 
-'''async def check_plan(bot):
+async def check_plan(bot):
   _time = datetime.now().strftime("%Y-%m-%d")
   all_data = await get_plan_data(_time)
   for data in all_data:
@@ -34,15 +34,15 @@ async def check_up(bot):
         x = await bot.send_message(chat_id=id, text=f"Subscription Expired ", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Buy Now", url=f"t.me/{OWNER}")]]))
         await bot.pin_chat_message(chat_id=id, message_id=x.id)
     except Exception as e:
-      await bot.send_message(OWNER_ID, f"Got error in Related Subscription Expired {e}\nUser : {data['user_name']}\nUser ID : {data['user_id']}\nChat ID :{data['_id']}\n")'''
+      await bot.send_message(OWNER_ID, f"Got error in Related Subscription Expired {e}\nUser : {data['user_name']}\nUser ID : {data['user_id']}\nChat ID :{data['_id']}\n")
     
 async def run_check_up():
     async with bot: 
         while True:  
            await check_up(bot)
-           '''await check_plan(bot)'''
+           await check_plan(bot)
            await asyncio.sleep(1)   
 
 bot.start()
-asyncio.create_task(run_check_up())
+asyncio.create_task(run_check_up())'''
 
