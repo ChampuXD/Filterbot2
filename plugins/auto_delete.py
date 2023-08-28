ec64dc5afb192e87abbef4f8c2d67040bb795be0
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-28 23:03:32 +0530

    Okie

--- plugins/auto_delete.py
+++ plugins/auto_delete.py
@@ -1,9 +1,9 @@
 from bot import Client
 from db import *
 from config import *
-from pyrogram import *
-from pyrogram.types import *
-from pyrogram.errors import *
+from pyrogram import filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
 
 
 @Client.on_message(filters.command("autodel"))
