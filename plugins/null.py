5b37127e667aa1b9e286618c13d698cf05f1c0d3
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-28 20:48:50 +0530

    okie

--- plugins/null.py
+++ plugins/null.py
@@ -1,19 +1,25 @@
-from bot import Client
-from pyrogram import *
-from pyrogram.types import *
+from bot import Client 
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 ADMIN = 1791227679
-@Client.on_message(filters.command("my"))
-async def my_handle(bot: Client, m: Message):
-  BUTTON = [[
-    InlineKeyboardButton("OWNER",callback_data="okie"),
-    InlineKeyboardButton("Coder",user_id=ADMIN)
-    ]]
-  await m.reply("Test Button",reply_markup=InlineKeyboardMarkup(BUTTON))
-  
+
+@Client.on_message(filters.command("my") & filters.user(ADMIN))
+async def my_handle(_, m):
+  BUTTON = [
+        [
+            InlineKeyboardButton("OWNER", callback_data="okie"),
+            InlineKeyboardButton("Coder", callback_data="coder")
+        ]
+    ]
+  await m.reply("Test Button", reply_markup=InlineKeyboardMarkup(BUTTON))
+
 @Client.on_callback_query()
-async def cb_me(_, q: CallbackQuery): 
-  data = q.data
-  if data == "okie": 
-    await q.message.edit("Just A Prank")
-  
+async def cb_me(client: Client, callback_query: CallbackQuery):
+    data = callback_query.data
+    if data == "okie":
+        await callback_query.edit_message_text("Just A Prank")
+    elif data == "coder":
+        user = await client.get_users(ADMIN)
+        await callback_query.answer(f"The coder's username is @{user.username}")
+        
