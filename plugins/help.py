b089b180e5758ba4c8a5c26515f3a08bf3812291
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-29 16:34:27 +0530

    Okie

--- plugins/help.py
+++ plugins/help.py
@@ -43,7 +43,7 @@ async def buy_handle(Client,m):
   await m.reply(text="All The Available Plans",reply_markup=BUTTON)
   
 @Client.on_callback_query()
-async def cb_help(Client, CallbackQuery):
+async def cb_help(Client, callback_query: CallbackQuery):
   data = callback_query.data
   PLAN_USD = '''These are the prices in USD:\n\n2 USD - per Month\n6 USD - per 6 Months\n10 USD - per Year\n\nClick on the Buy button to contact the owner'''
   PLAN_INR = '''These are the prices in INR:\n\n150 INR - per Month\n400 INR - per 6 Months\n800 INR - per Year\n\nClick on the Buy button to contact the owner'''
@@ -61,9 +61,9 @@ async def cb_help(Client, CallbackQuery):
     ])
     
   if data == "inr_p":
-    await CallbackQuery.message.edit(PLAN_INR, reply_markup=BTN_1)
+    await callback_query.message.edit(PLAN_INR, reply_markup=BTN_1)
   elif data == "usd_p":
-    await CallbackQuery.message.edit(PLAN_USD, reply_markup=BTN_2)
+    await callback_query.message.edit(PLAN_USD, reply_markup=BTN_2)
 
     
  
