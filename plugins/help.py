f9b5b8ed7b8b6459e1d936e9d9f0928818dd82ac
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-30 16:19:03 +0530

    Okie

--- plugins/help.py
+++ plugins/help.py
@@ -1,5 +1,6 @@
 from config import OWNER
 from pyrogram import filters, Client
+from config import *
 
 HELP_TEXT = f'''😇How To use  me
 
@@ -47,3 +48,15 @@ async def id_handle(bot:Client , m):
   else:
     None
   await m.reply(MSG)
+
+TEXT = f'''This bot is made by @{OWNER} A full time python developer
+
+Our channel :- @movie_artss
+Our selling channel :- @platimostore
+
+Want to make any kink of bot & tool dm @{OWNER}'''
+
+@Client.on_message(filters.command("about"))
+async def about_handle(_,m):
+  chat_id = m.chat.id
+  await m.reply(TEXT)
