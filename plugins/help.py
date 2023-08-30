041e4c30d55959e3517e1cb47fbcf79359ce621f
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-30 16:17:19 +0530

    Okie

--- plugins/help.py
+++ plugins/help.py
@@ -1,6 +1,5 @@
 from config import OWNER
 from pyrogram import filters, Client
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 HELP_TEXT = f'''😇How To use  me
 
@@ -33,41 +32,7 @@ Auto_delete : use /autodel command to enable or disable
 async def help_handler(Client, m):
   chat_id = m.chat.id
   await m.reply(HELP_TEXT)
-
-@Client.on_message(filters.command("buy")) 
-async def buy_handle(Client,m):
-  BUTTON = InlineKeyboardMarkup([[
-  InlineKeyboardButton(text="USD PRICE",callback_data="usd_p"),
-  InlineKeyboardButton(text="INR PRICE",callback_data="inr_p")
-  ]])
-  await m.reply(text="All The Available Plans",reply_markup=BUTTON)
   
-@Client.on_callback_query()
-async def cb_help(Client, callback_query: CallbackQuery):
-  data = callback_query.data
-  PLAN_USD = '''These are the prices in USD:\n\n2 USD - per Month\n6 USD - per 6 Months\n10 USD - per Year\n\nClick on the Buy button to contact the owner'''
-  PLAN_INR = '''These are the prices in INR:\n\n150 INR - per Month\n400 INR - per 6 Months\n800 INR - per Year\n\nClick on the Buy button to contact the owner'''
-  BTN_1 = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton(text="Buy", url=f"t.me/{OWNER}"),
-            InlineKeyboardButton(text="USD PRICE", callback_data="inr_p")
-        ]
-    ])
-  BTN_2 = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton(text="Buy", url=f"t.me/{OWNER}"),
-            InlineKeyboardButton(text="INR PRICE", callback_data="usd_p")
-        ]
-    ])
-    
-  if data == "inr_p":
-    await callback_query.message.edit(PLAN_INR, reply_markup=BTN_1)
-  elif data == "usd_p":
-    await callback_query.message.edit(PLAN_USD, reply_markup=BTN_2)
-
-    
- 
-
 @Client.on_message(filters.command("id"))
 async def id_handle(bot:Client , m):
   chat_id = m.chat.id
