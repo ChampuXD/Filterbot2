61e5db55a986b365303743a8f054f157e97f5eb5
yashattitude123@gmail.com <yashattitude123@gmail.com>
2023-08-30 17:08:18 +0530

    okie

--- bot.py
+++ bot.py
@@ -46,33 +46,3 @@ class Bot(Client):
             LOGGER.exception("Error while stopping bot: %s", str(e))
 
 
-@Client.on_message(filters.command("buy")) 
-async def buy_handle(Client,m):
-  BUTTON = InlineKeyboardMarkup([[
-  InlineKeyboardButton(text="USD PRICE",callback_data="usd_p"),
-  InlineKeyboardButton(text="INR PRICE",callback_data="inr_p")
-  ]])
-  await m.reply(text="All The Available Plans",reply_markup=BUTTON)
-  
-@Client.on_callback_query()
-async def cb_help(Client, callback_query: CallbackQuery):
-  data = callback_query.data
-  PLAN_USD = '''These are the prices in USD:\n\n2 USD - per Month\n6 USD - per 6 Months\n10 USD - per Year\n\nClick on the Buy button to contact the owner'''
-  PLAN_INR = '''These are the prices in INR:\n\n150 INR - per Month\n400 INR - per 6 Months\n800 INR - per Year\n\nClick on the Buy button to contact the owner'''
-  BTN_1 = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton(text="Buy", url=f"t.me/{OWNER}"),
-            InlineKeyboardButton(text="USD PRICE", callback_data="inr_p")
-        ]
-    ])
-  BTN_2 = InlineKeyboardMarkup([
-        [
-            InlineKeyboardButton(text="Buy", url=f"t.me/{OWNER}"),
-            InlineKeyboardButton(text="INR PRICE", callback_data="usd_p")
-        ]
-    ])
-    
-  if data == "inr_p":
-    await callback_query.message.edit(PLAN_INR, reply_markup=BTN_1)
-  elif data == "usd_p":
-    await callback_query.message.edit(PLAN_USD, reply_markup=BTN_2)
--- plugins/delete.py
+++ plugins/delete.py
@@ -41,6 +41,40 @@ async def run_check_up():
         while True:  
            await check_up(bot)
            await check_plan(bot)
+           
+           
+           
+           
+@bot.on_message(filters.command("buy")) 
+async def buy_handle(_, m):
+  BUTTON = InlineKeyboardMarkup([[
+  InlineKeyboardButton(text="USD PRICE",callback_data="usd_p"),
+  InlineKeyboardButton(text="INR PRICE",callback_data="inr_p")
+  ]])
+  await m.reply(text="All The Available Plans",reply_markup=BUTTON)
+  
+@bot.on_callback_query()
+async def cb_help(_, callback_query: CallbackQuery):
+  data = callback_query.data
+  PLAN_USD = '''These are the prices in USD:\n\n2 USD - per Month\n6 USD - per 6 Months\n10 USD - per Year\n\nClick on the Buy button to contact the owner'''
+  PLAN_INR = '''These are the prices in INR:\n\n150 INR - per Month\n400 INR - per 6 Months\n800 INR - per Year\n\nClick on the Buy button to contact the owner'''
+  BTN_1 = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(text="Buy", url=f"t.me/{OWNER}"),
+            InlineKeyboardButton(text="USD PRICE", callback_data="inr_p")
+        ]
+    ])
+  BTN_2 = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(text="Buy", url=f"t.me/{OWNER}"),
+            InlineKeyboardButton(text="INR PRICE", callback_data="usd_p")
+        ]
+    ])
+    
+  if data == "inr_p":
+    await callback_query.message.edit(PLAN_INR, reply_markup=BTN_1)
+  elif data == "usd_p":
+    await callback_query.message.edit(PLAN_USD, reply_markup=BTN_2)           
 
 bot.start()
 asyncio.create_task(run_check_up())
